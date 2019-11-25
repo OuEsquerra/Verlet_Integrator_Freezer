7940d764b832cf18e03c88458febb8e40008f14c
Aitorlb7 <45202115+Aitorlb7@users.noreply.github.com>
2019-11-25 22:36:32 +0100

    Adapted function (again)

--- SDLTest - Programming 1/Verlet.h
+++ SDLTest - Programming 1/Verlet.h
@@ -22,14 +22,18 @@ void Verlet(particle* prev_part, particle* output_part, Vec3d force, float dt)
 {
 
 	Vec3d drag_force;
+	// Original function to calculate the aerodinamic force
+
 	//drag_force.x = 0.5f * 1.225f * prev_part->speed.x * prev_part->speed.x * 3.14159265f / 2.0f * 0.47f * ((prev_part->speed.x < 0) ? 1 : -1) ;
 	//drag_force.y = 0.5f * 1.225f * prev_part->speed.y * prev_part->speed.y * 3.14159265f / 2.0f * 0.47f * ((prev_part->speed.y < 0) ? 1 : -1) ;
 	//drag_force.z = 0.5f * 1.225f * prev_part->speed.z * prev_part->speed.z * 3.14159265f / 2.0f * 0.47f * ((prev_part->speed.z < 0) ? 1 : -1) ;
 
-	drag_force.x = 0.00452193f * 0.45f * prev_part->speed.x * prev_part->speed.x * ((prev_part->speed.x < 0) ? 1 : -1);
-	drag_force.y = 0.00452193f * 0.45f * prev_part->speed.y * prev_part->speed.y * ((prev_part->speed.x < 0) ? 1 : -1);
-	drag_force.z = 0.00452193f * 0.45f * prev_part->speed.z * prev_part->speed.z * ((prev_part->speed.x < 0) ? 1 : -1);
+	// Modified function so it fits with the higher values we are currently using
 
+	drag_force.x = 0.452193f * 0.2f * prev_part->speed.x * prev_part->speed.x * ((prev_part->speed.x < 0) ? 1 : -1) * dt;
+	drag_force.y = 0.452193f * 0.2f * prev_part->speed.y * prev_part->speed.y * ((prev_part->speed.x < 0) ? 1 : -1) * dt;
+	drag_force.z = 0.452193f * 0.2f * prev_part->speed.z * prev_part->speed.z * ((prev_part->speed.x < 0) ? 1 : -1) * dt;
+	
 	Vec3d new_acc;
 	new_acc.x = (force.x + drag_force.x) / prev_part->mass ;
 	new_acc.y = (force.y + drag_force.y) / prev_part->mass ;
